47dfb8746ad63b7cf39d2b67ea6c545e83228166
nadyash83 <bezborodova@pkkb1.ru>
2023-12-13 23:03:45 +1000

    Исправить код в Channel

--- src/channel.py
+++ src/channel.py
@@ -10,14 +10,17 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = 'UCwHL6WHUarjGfUM_586me8w'  # HighLoad Channel
+        self.channel_id = channel_id  # HighLoad Channel
         self.channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+
+    def printj(self, dict_to_print: dict) -> None:
+        """Выводит словарь в json-подобном удобном формате с отступами"""
+        print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         self.printj(self.channel)
 
-    def printj(self,dict_to_print: dict) -> None:
-        """Выводит словарь в json-подобном удобном формате с отступами"""
-        print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
+
 
